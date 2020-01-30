45841c08596a04196996ee5fd7fa2bb38ae20533
MironovS <stasmironovweb@gmail.com>
2020-01-30 11:33:55 +0300

    Lessons_7

---
+++ Lessons_7/js/main.js
@@ -0,0 +1,27 @@
+let startBtn = document.getElementById("start"),
+ inputBudget = document.getElementsByClassName("budget-value"),
+ inputDayBudget = document.getElementsByClassName("daybudget-value"),
+ inputLevel = document.getElementsByClassName("level-value"),
+ inputExpenses = document.getElementsByClassName("expenses-value"),
+ inputOptionalexpenses = document.getElementsByClassName("optionalexpenses-value"),
+ inputIncome = document.getElementsByClassName("income-value"),
+ inputMonthsavings = document.getElementsByClassName("monthsavings-value"),
+ inputYearsavings = document.getElementsByClassName("yearsavings-value"),
+ inputItem = document.querySelectorAll('.expenses-item'),
+ acceptBtnOne = document.getElementsByTagName('button')[0],
+ acceptBtnTwo = document.getElementsByTagName('button')[1],
+ acceptBtnResult = document.getElementsByTagName('button')[2],
+ optionaInput = document.querySelectorAll('.optionalexpenses-item'),
+ chooseIncome = document.querySelector('.choose-income'),
+ savings = document.getElementById("savings"),
+ chooseSum = document.querySelector('.choose-sum'),
+ choosePercent = document.querySelector('.choose-percent'),
+ yearValue = document.querySelector('.year-value'),
+ monthValue = document.querySelector('.month-value'),
+ dayValue = document.querySelector('.day-value');
+ 
+ 
+
+
+
+ console.log(dayValue);
